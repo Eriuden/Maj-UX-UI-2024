df2ff0fd6d9a88cba89b2ba466e3fd0045c636fb
Eriuden <antoinecarrette@hotmail.fr>
2024-03-24 11:24:31 +0100

    setup 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import './App.css'
+import ButtonGradient from "./assets/svg/ButtonGradient"
+import { Button } from './components/Button'
 
 const App = () => {
   
@@ -7,8 +9,15 @@ const App = () => {
       <h1 className='text-3xl font-bold underline'>
         Hello world!
       </h1> 
-    </>
-      
+
+      <ButtonGradient/>
+
+      <div className='pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden'>
+        <Button className="mt-10" href="#login">
+          Bouton
+        </Button>
+      </div>
+    </>    
   )
 }
 
