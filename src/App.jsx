12119cf2dc50869b0c59c78c14099014f6fbe80b
Eriuden <antoinecarrette@hotmail.fr>
2024-03-24 10:45:58 +0100

    setup 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css'
 
-function App() {
+const App = () => {
   
   return (
-    <h1 className='text-3xl font-bold underline'>
-      Hello world!
-    </h1>   
+    <>
+      <h1 className='text-3xl font-bold underline'>
+        Hello world!
+      </h1> 
+    </>
+      
   )
 }
 
