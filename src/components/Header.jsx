bcfdf46ce74ea6ac0ef016e323935ba397d87ad2
Eriuden <antoinecarrette@hotmail.fr>
2024-03-25 11:39:17 +0100

    header 2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,8 +8,18 @@ export const Header = () => {
         <div className="flex items-center px-5 lg:px-7.5 
         xl:px-10 max-lg:py-4">
             <a className="block w-[12rem] xl:mr-8" href="#hero">
-                <img src={brainwave}/>
+                <img src={brainwave} width={190} height={40}/>
             </a>
+
+            <nav className="hidden fixed top-[5rem] left-0
+            right-0 bottom-0 bg-n-8 lg:static lg:flex
+            lg:mx-auto lg:bg-transparent">
+                <div className="relative z-2 flex flex-col
+                items-center justify-center m-auto 
+                lg:flex-row">
+                    
+                </div>
+            </nav>
         </div>
     </div>
   )
