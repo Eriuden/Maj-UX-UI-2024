1cad2bce5d571dcdaf8d602d770f4fa9a303716c
Eriuden <antoinecarrette@hotmail.fr>
2024-03-25 16:02:06 +0100

    header 7

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,23 +2,29 @@ import { useLocation } from "react-router-dom"
 import {brainwave} from "../assets"
 import { navigation } from "../constants"
 import { Button } from "./Button"
+import MenuSvg from "../assets/svg/MenuSvg"
+import { HamburgerMenu } from "./design/Header"
+import { useState } from "react"
 
 export const Header = () => {
   const pathName = useLocation()
+  const [openNavigation, setOpenNavigation] = useState(false)
 
   return (
-    <div className="fixed top-0 left-0 w-full z-50 bg-n-8/90
+    <div className={`fixed top-0 left-0 w-full z-50 bg-n-8/90
     backdrop-blur-sm border-b border-n-6 lg:bg-n-8/90
-    lg:backdrop-blur-sm">
+    lg:backdrop-blur-sm ${openNavigation ? "bg-n-8" 
+    : "bg-n-8/90 backdrop-blur-sm"} `}>
         <div className="flex items-center px-5 lg:px-7.5 
         xl:px-10 max-lg:py-4">
             <a className="block w-[12rem] xl:mr-8" href="#hero">
                 <img src={brainwave} width={190} height={40}/>
             </a>
 
-            <nav className="hidden fixed top-[5rem] left-0
+            <nav className={`${openNavigation ? "flex" : "hidden"} 
+            fixed top-[5rem] left-0
             right-0 bottom-0 bg-n-8 lg:static lg:flex
-            lg:mx-auto lg:bg-transparent">
+            lg:mx-auto lg:bg-transparent`}>
                 <div className="relative z-2 flex flex-col
                 items-center justify-center m-auto 
                 lg:flex-row">
@@ -37,6 +43,8 @@ export const Header = () => {
                             {item.title}
                         </a>
                     ))}
+
+                    <HamburgerMenu/>
                 </div>
             </nav>
 
@@ -47,6 +55,10 @@ export const Header = () => {
 
             <Button className="hidden lg:flex" href="#login">
                 Inscription
+            </Button>
+
+            <Button className="ml-auto lg:hidden">
+                <MenuSvg />
             </Button>
         </div>
     </div>
