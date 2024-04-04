531ed1a6f510543d539a7d4b4a4a3de4d455a9ab
Eriuden <antoinecarrette@hotmail.fr>
2024-04-04 11:34:16 +0200

    benefits 3

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -3,6 +3,8 @@ import { benefits } from '../constants'
 import { Heading } from './Heading'
 import { Section } from './Section'
 import Arrow from "../assets/svg/Arrow"
+import GradientLight from "./design/Benefits"
+import ClipPath from "../assets/svg/ClipPath"
 
 export const Benefits = () => {
   return (
@@ -19,7 +21,7 @@ export const Benefits = () => {
                      key={item.id}>
                         <div className='relative z-2 flex flex-col
                         min-h-[22rem] p-[2.4rem] pointer-events-none'>
-                            <h5 className='h2 mb-5'>{item.title}</h5>
+                            <h5 className='h5 mb-5'>{item.title}</h5>
                             <p className='body-2 mb-6 text-n-3'>{item.text}</p>
                             <div className='flex items-center mt-auto'>
                                 <img src={item.iconUrl} 
@@ -32,6 +34,23 @@ export const Benefits = () => {
                                 <Arrow/>
                             </div>
                         </div>
+
+                        {item.light && <GradientLight/>}
+
+                        <div className='absolute inset-0.5 bg-n-8' 
+                        style={{ clipPath: "url(#benefits)"}}>
+                            <div className='absolute inset-0 opacity-0
+                            transition-opacity hover:opacity-10'>
+                                {item.imageUrl && (
+                                    <img src={item.imageUrl}
+                                    height={380} width={362}
+                                    alt={item.title}
+                                    className="w-full h-full 
+                                    object-cover"/>
+                                )}
+                            </div>                       
+                        </div>
+                        <ClipPath/>
                     </div>
                 ))}
             </div>
