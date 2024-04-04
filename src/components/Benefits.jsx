457069d60bc448978c80cd10dd5d845bbdc583cc
Eriuden <antoinecarrette@hotmail.fr>
2024-04-04 10:54:28 +0200

    benefits 1

---
+++ src/components/Benefits.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Heading } from './Heading'
+import { Section } from './Section'
+
+export const Benefits = () => {
+  return (
+    <Section id="features">
+        <div className='container relative z-2'>
+            <Heading className="md:max-w-md lg:max-w-2xl" title="La solution pour
+            converser"/>
+
+            <div className='flex flex-wrap gap-10 mb-10'>
+                
+            </div>
+        </div>
+    </Section>
+  )
+}
