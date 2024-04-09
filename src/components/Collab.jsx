67b9ec5d189621599efd5b001935b04206a70328
Eriuden <antoinecarrette@hotmail.fr>
2024-04-09 17:37:08 +0200

    collab 2

--- src/components/Collab.jsx
+++ src/components/Collab.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { collabContent } from '../constants'
+import { collabContent, collabText } from '../constants'
+import { Button } from './Button'
 import { Section } from './Section'
 
 export const Collab = () => {
@@ -15,10 +16,23 @@ export const Collab = () => {
                                 <img src={check} width={24} height={24} 
                                 alt="check"/>
                                 <h6 className='body-2 ml-5'>{item.title}</h6>
+                                
                             </div>
+                            {item.text && (
+                                <p className='body-2 mt-3 text-n-4'>
+                                    {item.text}
+                                </p>
+                            )}
                         </li>
                     ))}
                 </ul>
+
+                <Button>Faites un essai</Button>
+            </div>
+
+            <div className='lg:ml-auto xl:w-[38rem]'>
+                <p className='body-2 mb-4 text-n-4 md:mb-16 lg:mb-32
+                lg:w-[22rem] lg:mx-auto'>{collabText}</p>
             </div>
         </div>
     </Section>
