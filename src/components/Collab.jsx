ddd58f59f9ad831e40b5745621ee20f2b703cffe
Eriuden <antoinecarrette@hotmail.fr>
2024-04-09 17:59:21 +0200

    collab 4

--- src/components/Collab.jsx
+++ src/components/Collab.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { brainwaveSymbol } from '../assets'
-import { collabContent, collabText } from '../constants'
+import { collabApps, collabContent, collabText } from '../constants'
 import { Button } from './Button'
 import { Section } from './Section'
 
@@ -49,6 +49,23 @@ export const Collab = () => {
                             </div>
                         </div>
                     </div>
+
+                    <ul>
+                        {collabApps.map((app,index)=> (
+                            <li key={app.id} className={`absolute top-0
+                            left-1/2 h-1/2 -ml-[1.6rem] origin-bottom
+                            rotate-${index * 45}`}>
+                                <div className={`relative -top-[1.6rem] 
+                                flex-w-[3.2rem] h-[3.2rem] bg-n-7 
+                                border border-n-1/15 rounded-xl 
+                                -rotate-${index * 45}`}>
+                                    <img className='m-auto' width={app.width}
+                                    height={app.height} alt={app.title}
+                                    src={app.icon} />
+                                </div>
+                            </li>
+                        ))}
+                    </ul>
 
                  </div>
             </div>
