bb2c678f5b405582d4666c54fc6611b46b656728
Eriuden <antoinecarrette@hotmail.fr>
2024-04-09 17:47:36 +0200

    collab 3

--- src/components/Collab.jsx
+++ src/components/Collab.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { brainwaveSymbol } from '../assets'
 import { collabContent, collabText } from '../constants'
 import { Button } from './Button'
 import { Section } from './Section'
@@ -33,6 +34,23 @@ export const Collab = () => {
             <div className='lg:ml-auto xl:w-[38rem]'>
                 <p className='body-2 mb-4 text-n-4 md:mb-16 lg:mb-32
                 lg:w-[22rem] lg:mx-auto'>{collabText}</p>
+
+                <div className='relative left-1/2 flex-w-[22rem] aspect-square
+                 border border-n-6 rounded-full -translate-x-1/2 scale-75
+                 md:scale-100'>
+                    <div className='flex w-60 aspect-square m-auto border
+                    border-n-6 rounded-full'>
+                        <div className='w-[6rem] aspect-square m-auto
+                        p-[0.2rem] bg-conic-gradient rounded-full'>
+                            <div className='flex items-center justify-center w-full
+                            h-full bg-n-8 rounded-full'>
+                                <img src={brainwaveSymbol}
+                                 height={48} width={48} alt="Symbole"/>
+                            </div>
+                        </div>
+                    </div>
+
+                 </div>
             </div>
         </div>
     </Section>
