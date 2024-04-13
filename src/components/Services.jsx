84cadd87b25abe26ac2f7f7b6d82d6da8ff5604e
Eriuden <antoinecarrette@hotmail.fr>
2024-04-13 09:59:38 +0200

    services 3

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import { service1 } from '../assets'
+import { service1, service2 } from '../assets'
 import { brainwaveServices } from '../constants'
+import { Generating } from './Generating'
 import { Heading } from './Heading'
 import { Section } from './Section'
 
@@ -38,6 +39,32 @@ export const Services = () => {
                                 </li>
                             ))}
                         </ul>
+                    </div>
+                    <Generating className='absolute left-4 
+                    right-4 bottom-4 border-n-1/10 border
+                    lg:left-1/2 lg-right-auto lg:bottom-8
+                    lg:-translate-x-1/2' />
+                </div>
+
+                <div className='relative z-1 grid gap-5
+                lg:grid-cols-2'>
+                    <div className='relative min-h-[39rem]
+                    border border-n-1/10 rounded-3xl
+                    overflow-hidden'>
+                        <div className='absolute inset-0'>
+                            <img src={service2} className="h-full
+                            w-full object-cover" width={630}
+                            height={750} alt="robot" />
+                        </div>
+
+                        <div className='absolute inset-0 flex flex-col
+                        justify-end p-8 bg-gradient-to-b
+                        from-n-8/0 to-n-8/90 lg:p-15'>
+                            <h4 className='h4 mb-4'>IA perfectionnée</h4>
+                            <p className='body-2 mb-[3rem] text-n-3'>Débloquez
+                            le potentiel des applications fonctionnant avec l'IA
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
